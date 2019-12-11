9b123c1feeba6267974a94adf1e50073b2248b4c
Steven Nemetz <steven@appzen.com>
2019-12-11 15:19:06 -0800

    Remove what lecacy account cannot handle

--- alias.tf
+++
@@ -1,7 +0,0 @@
-//
-// Manage account alias (IAM)
-//
-resource "aws_iam_account_alias" "alias" {
-  provider      = "aws.member"
-  account_alias = "${var.account_name}"
-}
--- s3.tf
+++
@@ -1,8 +0,0 @@
-# Account setting
-resource "aws_s3_account_public_access_block" "account" {
-  provider                = "aws.member"
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
-}
